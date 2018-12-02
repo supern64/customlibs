bc405eae16a387f102123477307d9246e4248a95
chanonlim <41817899+chanonlim@users.noreply.github.com>
2018-12-02 17:31:28 +0700

    added css generator

--- cssparser.py
+++ cssparser.py
@@ -32,3 +32,28 @@ def parse_text(string):
             name = property.name
             parsed[selectorText][name] = property.value
     return parsed
+def construct_css_string(dic):
+    """Constructs a CSS formatted string from a dictionary"""
+    string = ""
+    for i, (k, v) in enumerate(dic.items()):
+        if i == 0:
+            string = string + "." + k + " {\n"
+        else:
+            string = string + "\n." + k + " {\n"
+        for s, (c, j) in enumerate(v.items()):
+            string = string + "    " + c + ": " + j + ";\n"
+        string = string + "}"
+    return string
+def construct_css_to_file(dic, filename):
+    """Constructs a CSS formatted string from a dictionary then saves it to a file."""
+    string = ""
+    for i, (k, v) in enumerate(dic.items()):
+        if i == 0:
+            string = string + "." + k + " {\n"
+        else:
+            string = string + "\n." + k + " {\n"
+        for s, (c, j) in enumerate(v.items()):
+            string = string + "    " + c + ": " + j + ";\n"
+        string = string + "}"
+    open(filename, "w+").write(string)
+    return
