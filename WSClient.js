3cc46498108815bcd36233941c139056217db015
chanonlim <41817899+chanonlim@users.noreply.github.com>
2019-01-21 12:27:24 +0000

    Update WSClient.js

--- WSClient.js
+++ WSClient.js
@@ -1,4 +1,6 @@
 // You must have https://github.com/Olical/EventEmitter on your website for this client to work
+
+// Enumerations
 window.WSState = {
   CONNECTING: 0,
   OPEN: 1,
@@ -11,8 +13,40 @@ window.WSStateMap = {
   2: "CLOSING",
   3: "CLOSED"
 }
+window.WSCloseReason = {
+  NORMAL: 1000,
+  GOING_AWAY: 1001,
+  PROTOCOL_ERROR: 1002,
+  UNPROCESSABLE_INPUT: 1003,
+  RESERVED: 1004,
+  NOT_PROVIDED: 1005,
+  ABNORMAL: 1006,
+  INVALID_DATA: 1007,
+  POLICY_VIOLATION: 1008,
+  MESSAGE_TOO_BIG: 1009,
+  EXTENSION_REQUIRED: 1010,
+  INTERNAL_SERVER_ERROR: 1011
+  TLS_HANDSHAKE_FAILED: 1015
+}
+window.WSCloseDescription = {
+  1000: 'Normal connection closure',
+  1001: 'Remote peer is going away',
+  1002: 'Protocol error',
+  1003: 'Unprocessable input',
+  1004: 'Reserved',
+  1005: 'Reason not provided',
+  1006: 'Abnormal closure, no further detail available',
+  1007: 'Invalid data received',
+  1008: 'Policy violation',
+  1009: 'Message too big',
+  1010: 'Extension requested by client is required',
+  1011: 'Internal Server Error',
+  1015: 'TLS Handshake Failed' 
+}
 Object.freeze(window.WSState)
 Object.freeze(window.WSStateMap)
+Object.freeze(window.WSCloseReason)
+Object.freeze(window.WSCloseDescription)
 function getStateFromNumber(num) { return WSStateMap[num] }
 
 class WSClient extends EventEmitter {
@@ -55,7 +89,7 @@ class WSClient extends EventEmitter {
       this._client.close(code, reason)
       return
     } else if (code && !reason) {
-      this._client.close(code)
+      this._client.close(code, WSCloseDescription[code])
       return
     } else if (reason && !code) {
       throw new Error("Cannot close WebSocket w/ reason but w-o/ code")
