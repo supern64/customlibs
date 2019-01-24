81fa077b2e6164360de733f812c040dd166bfdf7
chanonlim <41817899+chanonlim@users.noreply.github.com>
2019-01-24 11:38:30 +0000

    Create new file

---
+++ delimlib.js
@@ -0,0 +1,28 @@
+const isNode = typeof process !== 'undefined' && process.versions != null && process.versions.node != null;
+
+function parse(text, delimeter=":", newlineDelimeter="\n") {
+	if (delimeter == newlineDelimeter) {
+		throw new Error("Delimeter cannot be the same as newline delimeter")
+	}
+    var parseData = text.split(newlineDelimeter)
+    var returnValue = {}
+    for (var i in parseData) {
+        var b = parseData[i].split(delimeter)
+        returnValue[b[0].trim()] = b[1].trim()
+    }
+    return returnValue
+}
+function construct(object, delimeter=": ", newlineDelimeter="\n") {
+	if (delimeter == newlineDelimeter) {
+		throw new Error("Delimeter cannot be the same as newline delimeter")
+	}
+    var array = []
+    for (var key in object) {
+        array.push(key + delimeter + object[key])
+    }
+    return array.join(newlineDelimeter)
+}
+if (isNode) {
+    module.exports.parse = parse
+    module.exports.construct = construct
+}
