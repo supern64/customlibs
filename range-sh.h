f6e931826478eef6dd842d940fea8724e7fae796
Chanon Limudomporn <41817899+chanonlim@users.noreply.github.com>
2019-12-14 12:39:05 +0700

    Did a tiny thing and made a header-only version

---
+++ range-sh.h
@@ -0,0 +1,49 @@
+#ifndef RANGE_H
+#define RANGE_H
+
+namespace range
+{
+    class Range {
+        private:
+			int start;
+			int end;
+			
+		public:
+            inline Range(int start, int end): start(start), end(end){};
+			
+			inline bool isInRange(int number)
+            {
+                if (number >= start && number <= end)
+		        {
+			        return true;
+		        } else 
+		        {
+			        return false;
+		        }
+            };
+
+			inline bool isInRange(Range range)
+            {
+                if (range.start >= start && range.end <= range.end)
+		        {
+			        return true;
+		        } else
+		        { 
+			        return false;
+		        }
+            }
+
+			inline bool isOverlapping(Range range)
+            {
+                if (isInRange(range.start) || isInRange(range.end))
+		        {
+			        return true;
+		        } else
+		        { 
+			        return false;
+		        }
+            }
+    };
+}
+
+#endif 
