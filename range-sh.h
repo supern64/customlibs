8b5ea387fdb8a0277d330082d788f9be093a34f8
Chanon Limudomporn <41817899+chanonlim@users.noreply.github.com>
2019-12-14 12:40:14 +0700

    Made a cleaner copy

--- range-sh.h
+++ range-sh.h
@@ -5,43 +5,43 @@ namespace range
 {
     class Range {
         private:
-			int start;
-			int end;
-			
-		public:
+            int start;
+            int end;
+            
+        public:
             inline Range(int start, int end): start(start), end(end){};
-			
-			inline bool isInRange(int number)
+            
+            inline bool isInRange(int number)
             {
                 if (number >= start && number <= end)
-		        {
-			        return true;
-		        } else 
-		        {
-			        return false;
-		        }
+                {
+                    return true;
+                } else 
+                {
+                    return false;
+                }
             };
 
-			inline bool isInRange(Range range)
+            inline bool isInRange(Range range)
             {
                 if (range.start >= start && range.end <= range.end)
-		        {
-			        return true;
-		        } else
-		        { 
-			        return false;
-		        }
+                {
+                    return true;
+                } else
+                { 
+                    return false;
+                }
             }
 
-			inline bool isOverlapping(Range range)
+            inline bool isOverlapping(Range range)
             {
                 if (isInRange(range.start) || isInRange(range.end))
-		        {
-			        return true;
-		        } else
-		        { 
-			        return false;
-		        }
+                {
+                    return true;
+                } else
+                { 
+                    return false;
+                }
             }
     };
 }
